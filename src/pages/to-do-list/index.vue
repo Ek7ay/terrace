941cac2842d51bedf200ea720ebbfdced94b892f
EK <qxjj7ay@163.com>
2020-07-24 22:06:21 +0800

    to-do-list写完

--- src/pages/to-do-list/index.vue
+++ src/pages/to-do-list/index.vue
@@ -1,15 +1,54 @@
 <template>
-    <div>
-        to do list
+    <div class="toDoBox">
+        <input
+            type="text"
+            v-model="inputValue"
+        >
+        <el-button
+            type="primary"
+            @click="addToList"
+        >增加任务</el-button>
+        <list
+            :info="list"
+            @getIndex="deleteInputValue"
+        />
     </div>
 </template>
 
 <script>
+    import list from "./list";
     export default {
-        name: "index"
+        data () {
+            return {
+                inputValue: '',
+                list: []
+            }
+        },
+        components: {
+            list
+        },
+        methods: {
+            addToList () {
+                this.list.push(this.inputValue)
+                this.inputValue = ""
+            },
+            deleteInputValue (index) {
+                this.list.splice(index, 1)
+            }
+        }
     }
 </script>
 
 <style lang="less" scoped>
-
+    .toDoBox {
+        padding: 100px 0 0 100px;
+        input {
+            width: 300px;
+            height: 40px;
+            border: 1px solid #25a4bb;
+            outline: none;
+            border-radius: 5px;
+            padding-left: 20px;
+        }
+    }
 </style>
---
+++ src/pages/to-do-list/list.vue
@@ -0,0 +1,47 @@
+<template>
+    <ul>
+        <li
+            v-for="(item, index) in info"
+            :key="index"
+        >
+            {{item}}
+            <el-button
+                class="btn"
+                type="danger"
+                icon="el-icon-delete"
+                size="mini"
+                circle
+                @click="deleteList(index)"
+            ></el-button>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        props: {
+            info: {}
+        },
+        methods: {
+            deleteList (index) {
+                this.$emit("getIndex", index)
+            }
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+     ul {
+         margin-top: 20px;
+         li {
+             border-bottom: 1px solid black;
+             width: 400px;
+             height: 40px;
+             line-height: 40px;
+             .btn {
+                 float: right;
+                 margin-top: 8px;
+             }
+         }
+     }
+</style>
