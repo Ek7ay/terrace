c266f7855ef2721ee179ed89eb00be768c4575ff
EK <qxjj7ay@163.com>
2020-07-25 01:17:51 +0800

    格式化

--- src/serve/index.js
+++ src/serve/index.js
@@ -3,6 +3,6 @@ import ajax from './ajax'
 // 1. 定义基础路径
 /***** 登录界面接口 *********/
 export const pwdLogin = (userName, pwd) => ajax('login', {
-  userName,
-  pwd,
+    userName,
+    pwd,
 }, 'POST');
